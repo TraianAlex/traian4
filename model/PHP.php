0d70ad2a31297952c0ef58a5c3fcc9a03de2137f
TraianAlex <victor_traian@yahoo.com>
2015-06-04 23:39:28 -0400

    first commit on santa

--- model/PHP.php
+++ model/PHP.php
@@ -4,11 +4,10 @@ class PHP extends DB_Manager{
     
     public function extract_dic($word) {
         
-        $objDb = new PDO('mysql:host=localhost;dbname=remb4372_traian', 'remb4372_victor', 'naiart92');
-        $objDb->exec('SET CHARACTER SET utf8');
-
+        //$objDb->exec('SET CHARACTER SET utf8');
+        self::$instance = DB_Manager::get_instance($_POST)->database();
         $sql = "SELECT * FROM dictionary WHERE SUBSTRING(word, 1, 1) = ?";
-        $statement = $objDb->prepare($sql);
+        $statement = self::$instance->prepare($sql);
         $statement->execute(array(substr($word, 0, 1)));
         $search = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $search ?: false;
@@ -18,18 +17,7 @@ class PHP extends DB_Manager{
         
         self::$instance = DB_Manager::get_instance($_POST)->database();
         self::$instance->table = 'dictionary';
-        $count = self::$instance->select('word')->where('word', $word)->getAll();
+        $count = self::$instance->select('word')->where('word', $word)->get();
         return $count ?: false;
-//        $objDb = new PDO('mysql:host=localhost;dbname=remb4372_traian', 'remb4372_victor', 'naiart92');
-//        $objDb->exec('SET CHARACTER SET utf8');
-//
-//        $sql = "SELECT word FROM dictionary WHERE word = :w ";
-//        $statement = $objDb->prepare($sql);
-//        $statement->execute(array(':w' => $word));
-//        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-//        if($result){
-//           return $result;
-//        }
-//        return false;
     }
 }
